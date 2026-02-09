bf9a197ee60b297cbf24ff349f03ddb893e3a93d
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2026-02-09 14:11:28 +0000

    Aula 01 Pyhton

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+escolha = input("Escolha a operação: \nSoma - 1 \nSubtração - 2 \n Multiplicação - 3 \n Divisão - 4")
+
+soma = num1 + num2
+sub = num1-num2
+mult = num1 * num2
+div = num1 / num2
+
+
+ifescolha = 1
---
+++ ola.py
@@ -0,0 +1,7 @@
+# print(f"Hello World")
+# nome = input("Qual é o seu nome? ")
+# print(f"Olá,",nome,". Seja bem vindo") 
+
+nome = input("Olá, qual é o seu nome? ")
+sobrenome = input("E o seu sobrenome? ")
+print("Então seu nome completo deve ser",nome,sobrenome)
