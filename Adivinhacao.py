f69a7a1ed46606092e186249f27574d76dc21c57
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2026-02-24 12:31:45 +0000

    versao 3 jogo adivinha

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,16 +1,42 @@
-print('***************************')
-print('*******Jogo Adivinhação****')
-print('***************************')
+import random
 
-numero_secreto = 39
+numero_secreto = random.randrange(1, 31)
+total_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: ")
+print("***************************")
+print("****JOGO DA ADIVINHAÇÃO****")
+print("***************************")
+print("Bem vindo ao jogo da adivinhação (☞ ͡° ͜ʖ ͡°)☞")
 
-print("Seu número é:", chute_str)
+for rodada in range(1, total_tentativas + 1):
+    print("Tentativa {} de {} 💪". format(rodada, total_tentativas))
+    chute_str = input("Digite o seu número: ")
 
-chute = int(chute_str)
+    print("Seu número é:", chute_str)
 
-if(numero_secreto == chute):
-    print("Você acertou")
-else:
-    print("Você errou  :(")
+    chute = int(chute_str)
+
+    if(chute < 0 or chute  > 30):
+        print("você deve digitar um número entre 1 e 30!")
+        continue
+
+    acertou = chute ==  numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Você acertou ✅️")
+        break
+    else:
+        if(maior):
+            print("O seu chute foi maior que o número secreto ➕ 🤓☝️")
+     
+        elif(menor):
+            print("O seu chute foi menor que o número secreto 🤓☝️")
+    rodada = rodada + 1
+
+
+print("Fim de jogo 🐊")
+
+        
